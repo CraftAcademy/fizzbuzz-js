6f6f8712fcb886e904325badb24404909041ee1c
Jade <jywfang@gmail.com>
2018-02-09 20:53:30 +0100

    working specs and fizzbuzz program

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,14 +1,23 @@
 describe("FizzBuzz", function() {
+  var fb;
+
+  beforeEach(function() {
+    fb = new FizzBuzz();
+  });
+
   it("should do something", function() {
     expect(1).toEqual(1);
   });
 
   it("returns Fizz if divisible by three", function() {
-    expect(FizzBuzz(3)).toEqual("Fizz");
+    expect(fb.result(3)).toEqual("Fizz");
   });
 
   it("returns Buzz if divisible by five", function() {
-    expect(FizzBuzz(5)).toEqual("Buzz");
+    expect(fb.result(5)).toEqual("Buzz");
   });
-  
+
+  it("returns FizzBuzz if divisible by 15", function() {
+    expect(fb.result(15)).toEqual("FizzBuzz");
+  });
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -2,10 +2,27 @@ function FizzBuzz(){
 }
 
 FizzBuzz.prototype.divisibleByThree = function(num) {
-  (num%3 === 0);
+  return this.fullyDivisible(num, 3);
+};
 
-FizzBuzz.prototype.fizzBuzz = function(num) {
-  if (this.divisibleByThree(num)) {
-    return "Fizz"
-  };
+FizzBuzz.prototype.divisibleByFive = function(num) {
+  return this.fullyDivisible(num, 5);
+};
+
+FizzBuzz.prototype.divisibleByBoth = function(num) {
+  return this.fullyDivisible(num, 15);
+};
+
+FizzBuzz.prototype.result = function(num) {
+  if (this.divisibleByBoth(num)) {
+    return "FizzBuzz";
+  } else if (this.divisibleByFive(num)) {
+    return "Buzz";
+  } else if (this.divisibleByThree(num)){
+    return "Fizz";
+  }
+};
+
+FizzBuzz.prototype.fullyDivisible = function(num, divisor){
+  return (num % divisor === 0);
 };
