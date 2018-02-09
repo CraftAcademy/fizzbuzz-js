2463418bd021262ec0069a5c544a16478f21923f
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-09 16:34:23 +0100

    added fizzbuzz implementation

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -2,19 +2,23 @@
 // numbers which can divided by 15, 5 or 3, to return "FizzBuzz", "Buzz" and "Fizz"
 
 describe("FizzBuzz", function() {
-  it("returns 1 of number is 1", function() {
+  it("returns 1 if number is 1", function() {
     expect(FizzBuzz(1)).toEqual(1);
   });
 
-  it("returns Fizz of number is 3", function() {
+  it("returns 2 if number is 2", function() {
+    expect(FizzBuzz(2)).toEqual(2);
+  });
+
+  it("returns Fizz if number is 3", function() {
     expect(FizzBuzz(3)).toEqual("Fizz");
   });
 
-  it("returns Buzz of number is 5", function() {
+  it("returns Buzz if number is 5", function() {
     expect(FizzBuzz(5)).toEqual("Buzz");
   });
 
-  it("returns FizzBuzz of number is 15", function() {
+  it("returns FizzBuzz if number is 15", function() {
     expect(FizzBuzz(15)).toEqual("FizzBuzz");
   });
 });
