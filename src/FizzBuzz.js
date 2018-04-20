219daa9f97440efd359ed8dcc9436cee27969fcf
nader <alghazu@gmail.com>
2018-04-20 14:08:41 +0200

    refactored

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,13 +1,13 @@
 const FizzBuzz = {
   eval(number) {
-    if (number%15 ==0) {
-      return 'FizzBuzz'
-    } else if (number%3 == 0){
-      return 'Fizz'
-    } else if (number%5 == 0){
-      return 'Buzz'
+    if (number % 15 == 0) {
+      return 'FizzBuzz';
+    } else if (number % 3 == 0){
+      return 'Fizz';
+    } else if (number % 5 == 0){
+      return 'Buzz';
     } else {
-      return number
+      return number;
     }
   }
 };
